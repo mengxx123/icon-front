f9fb9bd2c12f07b16cc81a71fa496b254d836c03
cjh <1418503647@qq.com>
2018-04-22 12:57:21 +0800

    update api domain

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,8 @@
 let apiDomain
 if (process.env.NODE_ENV === 'production') {
-    apiDomain = 'http://apifont.yunser.com'
+    apiDomain = 'https://apifont.yunser.com'
 } else {
-    apiDomain = 'http://apifont.yunser.com'
+    apiDomain = 'https://apifont.yunser.com'
     // apiDomain = 'http://localhost:1027'
 }
 
