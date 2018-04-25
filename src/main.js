33d29a2e4d0525ee9ee055149d31556c98e4a1b3
cjh <1418503647@qq.com>
2018-04-26 01:27:07 +0800

    add web intent all

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
@@ -7,13 +5,10 @@ import ui from './components/index'
 import http from './util/http'
 import qs from 'qs'
 import storage from './util/storage'
-import YunserUI from 'yunser-ui-vue'
-import 'yunser-ui-vue/dist/yunser-ui.css'
 import './scss/main.scss'
 
 Vue.config.productionTip = false
 
-Vue.use(YunserUI)
 Vue.use(ui)
 
 Vue.prototype.$http = http
@@ -27,11 +22,3 @@ new Vue({
     template: '<App/>',
     components: {App}
 })
-
-// const renderer = require('vue-server-renderer').createRenderer()
-//
-// renderer.renderToString(app, (err, html) => {
-//     if (err) throw err
-//     console.log(html)
-//     // => <div data-server-rendered="true">Hello World</div>
-// })
--- src/views/All.vue
+++ src/views/All.vue
@@ -9,18 +9,21 @@
         </ul>
         <ui-popup popupClass="download-box" position="right" :open="downloadVisible" @close="closeDownload()">
             <ui-appbar title="下载">
+                <ui-icon-button icon="all_inclusive" slot="right" @click="link" title="用其他应用打开" />
                 <ui-icon-button icon="close" slot="left" @click="closeDownload" />
             </ui-appbar>
-            <div class="editor">
-                <div class="bg">
-                    <div class="line-row" v-for="n in 15" :style="{top: (320 / 16 * n) + 'px'}"></div>
-                    <div class="line-column" v-for="n in 15" :style="{left: (320 / 16 * n) + 'px'}"></div>
+            <div class="body">
+                <div class="editor">
+                    <div class="bg">
+                        <div class="line-row" v-for="n in 15" :style="{top: (320 / 16 * n) + 'px'}"></div>
+                        <div class="line-column" v-for="n in 15" :style="{left: (320 / 16 * n) + 'px'}"></div>
+                    </div>
+                    <div class="svg-box2" v-html="dlIcon.content"></div>
                 </div>
-                <div class="svg-box2" v-html="dlIcon.content"></div>
+                <ui-raised-button class="btn" label="SVG 下载" @click="downloadSvg" />
+                <!--<ui-raised-button class="btn" label="AI 下载" @click="closeDownload" />-->
+                <ui-raised-button class="btn" label="PNG 下载" @click="downloadPng" />
             </div>
-            <ui-raised-button class="btn" label="SVG 下载" @click="downloadSvg" />
-            <!--<ui-raised-button class="btn" label="AI 下载" @click="closeDownload" />-->
-            <ui-raised-button class="btn" label="PNG 下载" @click="downloadPng" />
         </ui-popup>
         <canvas id="canvas" style="display: none"></canvas>
     </my-page>
@@ -30,6 +33,7 @@
     import {apiDomain} from '../config'
     import svgUtil from '../util/svg'
     const saveAs = window.saveAs
+    const Intent = window.Intent
 
     export default {
         data () {
@@ -87,10 +91,28 @@
                 } else {
                     this.dlIcon = icon
                     this.downloadVisible = true
+                    let head = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">'
+                    let content = head + this.dlIcon.content
+                    svgUtil.svgToDataUrl(content, dataUrl => {
+                        this.dataUrl = dataUrl
+                    })
                 }
             },
             closeDownload() {
                 this.downloadVisible = false
+            },
+            link() {
+                let intent = new Intent({
+                    action: 'http://webintent.yunser.com/?',
+                    type: 'image/png',
+                    data: this.dataUrl
+                })
+                navigator.startActivity(intent, data => {
+                    console.log('成功')
+                    this.content = data
+                }, data => {
+                    console.log('失败')
+                })
             },
             downloadSvg() {
                 let head = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">'
